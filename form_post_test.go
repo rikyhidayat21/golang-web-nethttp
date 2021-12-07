d6e00a8126a58152855aaeca337901a58097bf65
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-12-07 19:34:37 +0700

    Form Post

---
+++ form_post_test.go
@@ -0,0 +1,41 @@
+package golang_web_nethttp
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	// kalo mau otomatis tanpa parsing manual -> request.PostFormValue("value")
+
+	firstName := request.PostForm.Get("first_name")
+	lastName := request.PostForm.Get("last_name")
+
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("first_name=Riky&last_name=Hidayat")
+	request := httptest.NewRequest("POST", "http://localhost:4000", requestBody)
+	request.Header.Add("content-type", "application/x-www-form-urlencoded")
+
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+
+
+}
