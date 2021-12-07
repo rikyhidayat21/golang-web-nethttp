e7efcb4943a57a65b7a9f775bbd0979eca7e27ca
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-12-07 10:37:27 +0700

    ServeMux likes Route

--- handler_test.go
+++ handler_test.go
@@ -27,3 +27,37 @@ func TestHandler(t *testing.T) {
 
 
 }
+
+func TestServeMux(t *testing.T) {
+	// inisialisasi NewServeMux
+	mux := http.NewServeMux()
+
+	// handle -> url, functionHandlernya || Sementara HandleFunc() -> Memasukan url, functionHandle(anonymous function) [Baca Functionnya saja]
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello World")
+	})
+
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hi memet")
+	})
+
+	mux.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Login page")
+	})
+
+	// URL PATTERN
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Image")
+	})
+	mux.HandleFunc("/images/thumbnails/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Thumbnails")
+	})
+
+	// inisialisasi server
+	server := http.Server{Addr: "localhost:4000", Handler: mux}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
