58779eda993ff4492816d5e4956f48ff5601ad1d
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-12-09 17:12:40 +0700

    Serve File atau menggunakan Golang Embed

---
+++ serve_file_test.go
@@ -0,0 +1,56 @@
+package golang_web_nethttp
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		http.ServeFile(w, r, "./resources/ok.html")
+	} else {
+		http.ServeFile(w, r, "./resources/notfound.html")
+	}
+}
+
+func TestServeFile(t *testing.T) {
+	server := http.Server{
+		Addr: "localhost:4000",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Menggunakan golang embed
+
+//go:embed resources/ok.html
+var resourceOk string
+
+//go:embed resources/notfound.html
+var resourceNotFound string
+
+func ServeFileEmbed(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		fmt.Fprint(w, resourceOk)
+	} else {
+		fmt.Fprint(w, resourceNotFound)
+	}
+}
+
+func TestServeFileEmbed(t *testing.T) {
+	server := http.Server{
+		Addr: "localhost:4000",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
